c06937fe28ff553c32915a8e3a0fa7f3416d4cb4
Eli Goldner <egoldner@brandeis.edu>
2024-07-16 18:35:43 -0400

    Can use a dir full of query files in addition to a concrete list, also aggregation of answer fix

--- src/cnlpt/cnlp_seqgen.py
+++ src/cnlpt/cnlp_seqgen.py
@@ -53,6 +53,10 @@ parser.add_argument(
     default=[],
     help="TSVs for now, JSON or whatever else eventually",
 )
+parser.add_argument(
+    "--query_dir",
+    help="TSVs for now, JSON or whatever else eventually",
+)
 parser.add_argument("--output_dir", type=str)
 
 name2path = {
@@ -78,13 +82,18 @@ def main() -> None:
         load_in_4bit=args.load_in_4bit, load_in_8bit=args.load_in_8bit
     )
 
-    model_answers: Deque[Tuple[str,]] = deque()
     # Can't really find an idiomatic way to do this with
     # Hf Datasets.  My guess is part of this has to
     # do with best practices (or lack thereof?)
     # around how to best pad/trunctate input length
     system_prompt = get_system_prompt(args.prompt_file)
-    fn_to_queries = {q_fn: get_queries(q_fn) for q_fn in args.query_files}
+    if len(args.query_files) > 0:
+        fn_to_queries = {q_fn: get_queries(q_fn) for q_fn in args.query_files}
+    elif args.query_dir is not None and len(os.listdir(args.query_dir)) > 0:
+        fn_to_queries = {q_fn: get_queries(q_fn) for q_fn in get_files(args.query_dir)}
+    else:
+        ValueError("Need at least one file to make queries")
+        fn_to_queries = {}
 
     def few_shot_with_examples(
         examples: Iterable[Tuple[str, str]]
@@ -108,7 +117,7 @@ def main() -> None:
             get_prompt = empty_prompt
     elif args.sample_document is not None and args.sample_answer is not None:
         example = get_document_level_example(args.sample_document, args.sample_answer)
-        if all(example):
+        if all(len(ex) > 0 for ex in example):
             get_prompt = few_shot_with_examples(examples=(example,))
         else:
             ValueError("Empty sample document and/or empty sample answer")
@@ -118,11 +127,11 @@ def main() -> None:
         get_prompt = zero_shot_prompt
     start = time()
     # auth tokens for things like Mixtral
-    tokenizer = AutoTokenizer.from_pretrained(final_path, use_auth_token=True)
+    tokenizer = AutoTokenizer.from_pretrained(final_path)  # , use_auth_token=True)
 
     model = AutoModelForCausalLM.from_pretrained(
         final_path,
-        use_auth_token=True,
+        # use_auth_token=True,
         device_map="auto",
         quantization_config=quantization_config,
         # apparently this isn't idiomatic and you're
@@ -133,6 +142,7 @@ def main() -> None:
     print(f"Loading model took {end-start} seconds")
     pathlib.Path(args.output_dir).mkdir(parents=True, exist_ok=True)
     for q_fn, queries in fn_to_queries.items():
+        model_answers: Deque[Tuple[str,]] = deque()
         for query in tqdm(queries):
             prompt_messages = get_prompt(system_prompt, query)
             input_ids = tokenizer.apply_chat_template(
@@ -145,10 +155,15 @@ def main() -> None:
                 outputs.detach().cpu().numpy()[:, input_ids.shape[1] :],
                 skip_special_tokens=True,
             )[0]
-            model_answers.append((" ".join(gen_text.strip().split()),))
+            # model_answers.append((" ".join(gen_text.strip().split()),))
+            model_answers.append((gen_text.strip(),))
         output_df = pd.DataFrame.from_records(model_answers, columns=["answers"])
-        out_fn = os.path.join(args.output_dir, os.path.basename(q_fn))
-        output_df.to_csv(f"{out_fn}.tsv", index=False, sep="\t")
+        out_fn = os.path.join(args.output_dir, os.path.basename(q_fn)) + ".txt"
+        # output_df.to_csv(f"{out_fn}.tsv", index=False, sep="\t")
+        with open(out_fn, mode="wt", encoding="utf-8") as out_f:
+            for answer in model_answers:
+                (answer_str,) = answer
+                out_f.write(answer_str + "\n")
     print("Finished writing results")
 
 
@@ -224,6 +239,11 @@ def few_shot_prompt(
         {"role": "user", "content": query},
     ]
     return messages
+
+
+def get_files(raw_dir: str) -> Iterable[str]:
+    for base_fn in os.listdir(raw_dir):
+        yield os.path.join(raw_dir, base_fn)
 
 
 if __name__ == "__main__":
